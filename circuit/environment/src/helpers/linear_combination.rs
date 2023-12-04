99f17dd9eb9fa213b22e14c504cd34eef7e8bd9a
Milias <francisco@truevoid.dev>
2023-12-04 23:55:57 +0100

    More public attributes

--- circuit/environment/src/helpers/linear_combination.rs
+++ circuit/environment/src/helpers/linear_combination.rs
@@ -23,10 +23,10 @@ use indexmap::{map::Entry, IndexMap};
 
 #[derive(Clone)]
 pub struct LinearCombination<F: PrimeField> {
-    constant: F,
-    terms: IndexMap<Variable<F>, F>,
+    pub constant: F,
+    pub terms: IndexMap<Variable<F>, F>,
     /// The value of this linear combination, defined as the sum of the `terms` and `constant`.
-    value: F,
+    pub value: F,
 }
 
 impl<F: PrimeField> LinearCombination<F> {
